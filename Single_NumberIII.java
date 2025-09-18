5dec0aae2365d7aaa891bbf1241507ec56ee5dec
Hasini E <hasini1530@gmail.com>
2025-09-18 19:06:32 +0530

    Single_NumberIII.java

---
+++ Single_NumberIII.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        int diffBit = xor & -xor;
+        int[] result = new int[2];
+        for (int num : nums) {
+            if ((num & diffBit) == 0) {
+                result[0] ^= num; 
+            } else {
+                result[1] ^= num; 
+            }
+        }
+
+        return result;
+    }
+}
