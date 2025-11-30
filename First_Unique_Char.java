a936787060b2594459fa02523b2cdb70312d333e
Hasini E <hasini1530@gmail.com>
2025-11-30 22:15:27 +0530

    Implement first unique character finder.java

---
+++ First_Unique_Char.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution
+{
+    public int firstUniqChar(String s)
+    {
+        int[] charCounts=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            charCounts[c-'a']++; 
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(charCounts[c-'a']==1) 
+                return i; 
+        }
+        return -1;
+    }
+}
