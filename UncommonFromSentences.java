f3a5a54bd7eaed5e8274467bd59be04a29af7748
E HASINI <hasini1530@gmail.com>
2026-02-06 21:58:47 +0530

    Implement method to find uncommon words from sentences

---
+++ UncommonFromSentences.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    public String[] uncommonFromSentences(String s1, String s2)
+    {
+
+        String[] words=(s1+" "+s2).split(" ");
+        List<String> ans=new ArrayList<>();
+        for(int i=0;i<words.length; i++)
+        {
+            int count=0;
+            for(int j=0;j<words.length;j++)
+            {
+                if(words[i].equals(words[j]))
+                    count++;
+            }
+            if(count==1) 
+                ans.add(words[i]);
+        }
+        return ans.toArray(new String[0]);
+    }
+}
