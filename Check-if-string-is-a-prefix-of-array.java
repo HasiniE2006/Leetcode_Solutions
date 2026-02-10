0deeeed4ff370ce66cabc305a822fa20b26b27df
E HASINI <hasini1530@gmail.com>
2026-02-10 15:09:15 +0530

    Implement prefix string check in Solution class

---
+++ Check-if-string-is-a-prefix-of-array.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public boolean isPrefixString(String s, String[] words) 
+    {
+        String temp="";
+        for(String word:words)
+        {
+            temp+=word;
+            if(temp.equals(s)) return true;
+        }
+        return false;
+    }
+}
