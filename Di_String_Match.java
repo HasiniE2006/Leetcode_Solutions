456aa340778e026d82d4553fdb2b0f55f05383b5
E HASINI <hasini1530@gmail.com>
2026-07-19 19:05:29 +0530

    Implement diStringMatch function in Solution class

---
+++ Di_String_Match.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+
+        int low = 0;
+        int high = s.length();
+
+        int[] ans = new int[s.length() + 1];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == 'I') {
+                ans[i] = low;
+                low++;
+            } else {
+                ans[i] = high;
+                high--;
+            }
+        }
+
+        ans[s.length()] = low;
+
+        return ans;
+    }
+}
