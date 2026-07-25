dbbd84d4048dcd852ce4c0ce1ed47e036b467ef2
E HASINI <hasini1530@gmail.com>
2026-07-25 21:20:24 +0530

    Implement matrix transpose function in Solution class

---
+++ transpose.py
@@ -0,0 +1,11 @@
+class Solution:
+    def transpose(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+        ans = []
+        for j in range(cols):
+            temp = []
+            for i in range(rows):
+                temp.append(matrix[i][j])
+            ans.append(temp)
+        return ans
