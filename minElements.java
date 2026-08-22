3d8d913a2618ac58742ebdd3524e027850c56b46
E HASINI <hasini1530@gmail.com>
2026-08-22 22:35:26 +0530

    Implement minElements method to calculate adjustments

---
+++ minElements.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minElements(int[] nums, int limit, int goal) {
+        long sum = 0;
+        for (int x : nums)
+            sum += x;
+        long diff = Math.abs(goal - sum);
+        return (int)((diff + limit - 1) / limit);
+    }
+}
